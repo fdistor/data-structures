4e9648607c89eda3a2b63fce2c7053e5a5d651b6
Francis Distor <francis.distor@gmail.com>
2019-06-28 15:36:48 -0700

    write test for push

--- test/stack.test.js
+++ test/stack.test.js
@@ -22,3 +22,24 @@ describe("Stack class", () => {
     expect(stack.peek).to.be.a("function");
   });
 });
+
+describe("Stack functionality", () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  it("Should push a value into the stack", () => {
+    expect(stack.stack).to.be.empty;
+    stack.push(1);
+    expect(stack.stack[0]).to.equal(1);
+    stack.push(6);
+    expect(stack.stack[1]).to.equal(6);
+    expect(stack.stack[0]).to.equal(1);
+  });
+
+  it("Should remove the most recent item put into the stack", () => {});
+
+  it("Should return the most recent item put into the stack", () => {});
+});
